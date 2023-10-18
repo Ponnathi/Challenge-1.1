4239c2828b2d2f70fa938dfc647ea22ae2baecda
Ponnathi <148220682+Ponnathi@users.noreply.github.com>
2023-10-18 11:52:13 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,23 +1,32 @@
-class Player:
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
-  def play(self):
-    print("The player is playing cricket")
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            return f"Deposited ${amount}. New balance: ${self.__account_balance}"
+        else:
+            return "Invalid deposit amount. Amount must be greater than 0."
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return f"Withdrew ${amount}. New balance: ${self.__account_balance}"
+        else:
+            return "Invalid withdrawal amount or insufficient balance."
+
+    def display_balance(self):
+        return f"Account Balance for {self.__account_holder_name}: ${self.__account_balance}"
 
 
-class Batsman(Player):
+# Creating an instance of BankAccount
+account = BankAccount("1234567890", "John Doe", 1000.0)
 
-  def play(self):
-    print("The batsman is batting")
-
-
-class Bowler(Player):
-
-  def play(self):
-    print("The bowler is bowling")
-
-
-batsman = Batsman()
-bowler = Bowler()
-
-batsman.play()
-bowler.play()
+# Testing deposit and withdrawal functionality
+print(account.display_balance())
+print(account.deposit(500))
+print(account.withdraw(200))
+print(account.withdraw(1500))  # This should result in an error message
